d1732aaf70378c7b722387ca3553e0f5f150dfa3
ANILKUMARReg <anilregidi262@gmail.com>
2026-06-08 10:16:30 +0530

    Create React App component

---
+++ octofit-tracker/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>OctoFit Tracker</h1>
+      <p>Track your fitness journey with ease</p>
+    </div>
+  )
+}
+
+export default App
